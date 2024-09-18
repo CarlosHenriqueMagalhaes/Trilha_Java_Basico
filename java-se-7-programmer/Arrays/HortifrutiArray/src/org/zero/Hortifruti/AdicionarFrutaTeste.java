5c9d889023cc38ed8b829b13c44e35baa1f14c4c
Carlos Magalhaes <carlos.hmzero@gmail.com>
2024-09-18 09:57:23 -0300

    Adicionando novos itens na próxima posição null de uma Array

---
+++ java-se-7-programmer/Arrays/HortifrutiArray/src/org/zero/Hortifruti/AdicionarFrutaTeste.java
@@ -0,0 +1,7 @@
+package org.zero.Hortifruti;
+
+public class AdicionarFrutaTeste {
+    public static void main(String[] args) {
+        new Hortifruti().adicionarFruta();
+    }
+}
--- java-se-7-programmer/Arrays/HortifrutiArray/src/org/zero/Hortifruti/Hortifruti.java
+++ java-se-7-programmer/Arrays/HortifrutiArray/src/org/zero/Hortifruti/Hortifruti.java
@@ -26,7 +26,28 @@ public class Hortifruti {
             } else {
                 System.out.println("Escolha uma fruta válida!");
             }
+        }
+    }
 
+    public void adicionarFruta() {
+        System.out.print("Qual fruta será adicionada no Hortifruti? ");
+
+        Scanner leitura = new Scanner(System.in);
+        String novaFruta = leitura.nextLine();
+
+        // Encontre a próxima posição disponível na lista
+        int proximaPosicao = 0;
+        while (proximaPosicao < listaDeFrutas.length && listaDeFrutas[proximaPosicao] != null) {
+            proximaPosicao++;
+        }
+
+        // Adicione a fruta na próxima posição disponível
+        listaDeFrutas[proximaPosicao] = novaFruta;
+
+        System.out.println(novaFruta + " adicionado!");
+
+        for (int i = 0; i < listaDeFrutas.length; i++) {
+            System.out.println(i + ": " + listaDeFrutas[i]);
         }
     }
 }
