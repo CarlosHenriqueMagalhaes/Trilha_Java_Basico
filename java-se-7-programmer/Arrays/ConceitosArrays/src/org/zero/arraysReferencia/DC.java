f159d5e3e738557d1ae9df79e33acf38bce1412a
Carlos Magalhaes <carlos.hmzero@gmail.com>
2024-09-25 10:59:13 -0300

    Práticas - Arrays de referência

---
+++ java-se-7-programmer/Arrays/ConceitosArrays/src/org/zero/arraysReferencia/DC.java
@@ -0,0 +1,43 @@
+package org.zero.arraysReferencia;
+
+import org.zero.modelos.Personagem;
+import org.zero.modelos.Vilao;
+
+public class DC {
+    public static void main(String[] args) {
+        Personagem personagemDC[] = new Personagem[10];
+
+        personagemDC[0] = new Personagem();
+        personagemDC[0].nome = "Batman";
+
+        personagemDC[1] = new Personagem();
+        personagemDC[1].nome = "Superman";
+
+        personagemDC[2] = new Personagem();
+        personagemDC[2].nome = "Wonder Woman";
+
+        personagemDC[8] = new Personagem();
+        personagemDC[8].nome = "Ravena";
+
+        Personagem vilaoBatman = new Vilao();
+        vilaoBatman.nome = "Bane";
+        personagemDC[3] = vilaoBatman;
+
+        Personagem vilaoSuperman = new Vilao();
+        vilaoSuperman.nome = "Brainiac";
+        personagemDC[5] = vilaoSuperman;
+
+        System.out.println(personagemDC[0].nome);
+
+        System.out.println("-------DC Comics--------");
+
+        for (int i = 0; i < personagemDC.length; i++) {
+            if (personagemDC[i] != null) {
+                System.out.println(i + " - " + personagemDC[i].nome);
+            } else {
+                personagemDC[i] = new Personagem();
+                personagemDC[i].nome = " nenhum personagem cadastrado";
+            }
+        }
+    }
+}
