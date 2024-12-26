b42451c1f2e6dc2b9c8e53bb6be354506ed38339
Carlos Magalhaes <carlos.hmzero@gmail.com>
2024-12-26 10:51:27 -0300

    Java Servlet - Sessão: testando login - mandar para a tela de Login ao acessar a lista de empresa sem estar logado

--- java-web/ProgramacaoWebJava/Gerenciador/src/main/java/org/zero/gerenciador/empresas/acoes/ListaEmpresas.java
+++ java-web/ProgramacaoWebJava/Gerenciador/src/main/java/org/zero/gerenciador/empresas/acoes/ListaEmpresas.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.zero.gerenciador.empresas.modelo.BancoDeDadosGenerico;
 import org.zero.gerenciador.empresas.modelo.Empresa;
 
-public class ListaEmpresas implements Acao{
+public class ListaEmpresas implements Acao {
 	public String executa(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		HttpSession sessao = request.getSession();
+		if (sessao.getAttribute("usuarioLogado") == null) {
+			return "redirect:principal?acao=LoginForm";
+		}
+
 		System.out.println("Listando empresas");
 
 		BancoDeDadosGenerico listaBanco = new BancoDeDadosGenerico();
